ef907a4463ba2aafbac7752f3355618f76e23861
Ian <typppo@gmail.com>
2013-11-10 12:32:05 -0800

    working texting

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
+/**
+ * appe-monitor script
+ * MIT License
+ *
+ * @author Ian Webster (ianww.com)
+ */
+
 var request = require('request')
   , _ = require('underscore')
   , Q = require('q')
-  , querystring = require('querystring')
 
 var models_16g = {
   'ME341LL/A': 'iphone 5s space gray verizon 16gb',
   'ME343LL/A': 'iphone 5s gold verizon 16gb',
   'ME342LL/A': 'iphone 5s silver verizon 16gb',
+  // Add products you're interested in.
 };
 
-var ZIPCODE = 94043;
-var PHONE_NUMBER = null;   // '5555555555'
+if (process.argv.length < 3) {
+  console.log('usage: node apple.js zipcode');
+  process.exit(1);
+}
+
+var ZIPCODE = process.argv[2];
+var PHONE_NUMBER = null;
+if (process.argv.length > 3) {
+  PHONE_NUMBER = process.argv[3];
+}
 
 function check(url) {
   var deferred = Q.defer();
@@ -35,21 +50,23 @@ function checkStore(store, model, desc) {
 }
 
 function checkStores(stores, model, desc) {
+  var avail_stores  = [];
   var any = _.some(stores, function(store) {
-    return checkStore(store, model, desc);
+    var avail = checkStore(store, model, desc);
+    if (avail) {
+      avail_stores.push(store);
+    }
+    return avail;
   });
 
   if (any && PHONE_NUMBER) {
-    request.post({
-      uri:'http://textbelt.com/text',
-      headers:{'content-type': 'application/x-www-form-urlencoded'},
-      body: querystring.stringify({
-        number: ''+PHONE_NUMBER,
-        message: desc + ' is available in your area.',
+    request.post('http://textbelt.com/text',
+      { form: {
+          number: ''+PHONE_NUMBER,
+          message: desc + ' is available at ' + avail_stores[0].storeName,
+        }
       }, function(err, resp, body) {
-        console.log(arguments);
-      })
-    });
+      });
   }
 }
 
